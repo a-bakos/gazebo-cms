6160a3d937963eb348cd8c3609dc102115fd17b4
a-bakos <bakosattila@fastem.com>
2023-03-05 21:13:23 +0000

    Start simulating post creation, update, and storage

--- src/db.rs
+++ src/db.rs
@@ -1,11 +1,15 @@
 // Mock database
 use crate::consts;
+use crate::post::OX_Post;
 use csv::WriterBuilder;
 use std::error::Error;
 
-pub fn store() -> Result<(), Box<dyn Error>> {
-    println!("Writing results into CSV...");
+pub fn store(post: &OX_Post) -> Result<(), Box<dyn Error>> {
+    println!("Writing post data into CSV...");
     let mut wtr = WriterBuilder::new().from_path(consts::FILE_PATH)?;
+
+    //
+
     wtr.write_record(["1".to_string(), "2".to_string()])?;
     wtr.flush()?;
     Ok(())
--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,13 @@ mod user;
 use crate::post::{EntryType, OX_Post};
 
 fn main() {
+    // Start editing new post
     let mut post = OX_Post::draft(EntryType::Post);
+
+    // Update post with post data
+    // post = post.update();
+
     dbg!(&post);
 
-    let _store = db::store();
+    let _store = db::store(&post);
 }
