33ab17d14de84abea7aac05ea9c474e1e6cf0d80
a-bakos <bakosattila@fastem.com>
2023-03-09 22:38:11 +0000

    Get current date method

--- src/date.rs
+++ src/date.rs
@@ -1,7 +1,20 @@
 use chrono::prelude::*;
 
 pub fn get_current_date() -> String {
-    let utc: DateTime<Utc> = Utc::now();
-    let formatted = format!("{}", utc.format("%Y-%m-%d %H:%M:%S"));
-    formatted
+    let date = OX_DateTime::new();
+    date.formatted
+}
+
+struct OX_DateTime {
+    utc: DateTime<Utc>,
+    formatted: String,
+}
+
+impl OX_DateTime {
+    pub fn new() -> Self {
+        Self {
+            utc: Utc::now(),
+            formatted: format!("{}", utc.format("%Y-%m-%d %H:%M:%S")),
+        }
+    }
 }
