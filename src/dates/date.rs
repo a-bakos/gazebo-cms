2deac70cabbb0e6e7d1afcab3170e5df0a5d0350
Attila Bakos <bakosattila@fastem.com>
2023-03-19 19:57:23 +0000

    Add todo comments

--- src/dates/date.rs
+++ src/dates/date.rs
@@ -23,3 +23,9 @@ impl std::fmt::Display for OX_DateTime {
         write!(f, "{}", self.formatted)
     }
 }
+
+// Todo
+// define( 'MINUTE_IN_SECONDS', 60 );
+// define( 'HOUR_IN_SECONDS', 60 * MINUTE_IN_SECONDS );
+// define( 'DAY_IN_SECONDS', 24 * HOUR_IN_SECONDS );
+// define( 'WEEK_IN_SECONDS', 7 * DAY_IN_SECONDS );
