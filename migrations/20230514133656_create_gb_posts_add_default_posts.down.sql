4b09e1770e091644d85b23db7e69bb90396e1456
a-bakos <bakosattila@fastem.com>
2023-05-14 15:16:00 +0100

    Add command (w/ comments)  to roll back default posts

--- migrations/20230514133656_create_gb_posts_add_default_posts.down.sql
+++ migrations/20230514133656_create_gb_posts_add_default_posts.down.sql
@@ -1,1 +1,12 @@
--- Add down migration script here
+-- The *_down.sql commands are executed when you run a database migration rollback to undo the changes made by a previous migration.
+-- For example, if you have applied a migration that creates a new table in your database, and you want to roll back that migration,
+-- you would execute the sqlx migrate rollback command.
+-- This would run the *_down.sql script associated with the migration and remove the table from the database.
+-- The *_down.sql script should reverse the changes made by the corresponding *_up.sql script in the migration,
+-- to ensure that the rollback operation works correctly.
+
+DELETE FROM gb_posts WHERE title IN (
+    'Hello, Gazebo!',
+    'A CMS experiment project',
+    'Inspired by WordPress'
+);
