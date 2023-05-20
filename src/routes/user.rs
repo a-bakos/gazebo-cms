343705d7a1f4599042e19e6b78afbffe502044d8
a-bakos <bakosattila@fastem.com>
2023-05-20 10:44:51 +0100

    get_user() function is now working

--- src/routes/user.rs
+++ src/routes/user.rs
@@ -1,12 +1,11 @@
 use crate::users::roles::get_role_variant;
 use crate::users::user::{User, UserID};
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, FixedOffset, NaiveDateTime, Utc};
 use serde::{Deserialize, Serialize};
 use sqlx::postgres::{PgPool, PgRow};
+use sqlx::Error;
 use sqlx::Row;
 use std::num::ParseIntError;
-
-use sqlx::Error;
 use warp::reject::Reject;
 
 #[derive(Debug)]
@@ -15,6 +14,7 @@ struct SqlxError(Error);
 impl Reject for SqlxError {}
 
 // http://localhost:1337/user/{id}
+// when ID does not exist: Unhandled rejection: SqlxError(RowNotFound)
 pub async fn get_user(id: i32, pool: PgPool) -> Result<impl warp::Reply, warp::Rejection> {
     println!("User requested by ID: {}", id);
     match sqlx::query("SELECT * FROM gb_accounts WHERE id = $1")
@@ -22,19 +22,20 @@ pub async fn get_user(id: i32, pool: PgPool) -> Result<impl warp::Reply, warp::R
         .map(|row: PgRow| {
             let user_id: i32 = row.get("id");
             let user_id = user_id as u32;
+
             let user_role = row.get("role");
             let user_role = get_role_variant(user_role);
-            let registered: String = row.get("registered");
+
+            // Don't need to specify a default/fallback value because the cell will never be empty
+            let registered: NaiveDateTime = row.get::<NaiveDateTime, _>("registered");
 
             User {
-                first_name: String::from("FIRST"),
-                last_name: String::from("LAST"),
                 login_name: row.get("login"),
                 email: row.get("email"),
                 id: UserID(user_id),
                 role: user_role,
                 password: row.get("password"),
-                registered,
+                registered: registered.to_string(),
             }
         })
         .fetch_one(&pool)
