205ff1da832161054d3170fb86aecfec0521a660
a-bakos <bakosattila@fastem.com>
2023-05-22 06:45:39 +0100

    Create consts for column indexes + parameterize the table name in query

--- src/routes/user.rs
+++ src/routes/user.rs
@@ -1,3 +1,4 @@
+use crate::database::columns::COL_INDEX_USER_PASSWORD;
 use crate::users::roles::{get_role_variant, UserRole};
 use crate::users::user::{User, UserID};
 use chrono::{DateTime, FixedOffset, NaiveDateTime, Utc};
@@ -13,30 +14,40 @@ pub struct SqlxError(pub Error);
 
 impl Reject for SqlxError {}
 
+pub const COL_INDEX_ACCOUNT_ID: &str = "id";
+pub const COL_INDEX_ACCOUNT_EMAIL: &str = "email";
+pub const COL_INDEX_ACCOUNT_PASSWORD: &str = "email";
+pub const COL_INDEX_ACCOUNT_ROLE: &str = "role";
+pub const COL_INDEX_ACCOUNT_LOGIN: &str = "login";
+pub const COL_INDEX_ACCOUNT_REGISTERED: &str = "registered";
+
 // http://localhost:1337/user/{id}
 // when ID does not exist: Unhandled rejection: SqlxError(RowNotFound)
 pub async fn get_user_by_id(id: i32, pool: PgPool) -> Result<impl warp::Reply, warp::Rejection> {
+    println!("User requested by ID: {}", id);
     // TODO Authentication layer needs to be here.
     // if ! auth { return Err(warp::reject::custom(AuthError)), }
 
-    println!("User requested by ID: {}", id);
-    match sqlx::query("SELECT * FROM gb_accounts WHERE id = $1")
+    let table_name = "gb_accounts";
+    let query = format!("SELECT * FROM {} WHERE id = $1", table_name);
+    match sqlx::query(&query)
         .bind(id)
         .map(|row: PgRow| {
-            let user_id = row.get::<i32, _>("id") as u32;
+            let user_id = row.get::<i32, _>(COL_INDEX_ACCOUNT_ID) as u32;
 
-            let user_role = row.get::<&str, _>("role");
+            let user_role = row.get::<&str, _>(COL_INDEX_ACCOUNT_ROLE);
             let user_role = get_role_variant(user_role);
 
             // Don't need to specify a default/fallback value because the cell will never be empty
-            let registered: NaiveDateTime = row.get::<NaiveDateTime, _>("registered");
+            let registered: NaiveDateTime =
+                row.get::<NaiveDateTime, _>(COL_INDEX_ACCOUNT_REGISTERED);
 
             User {
-                login_name: row.get("login"),
-                email: row.get("email"),
+                login_name: row.get(COL_INDEX_ACCOUNT_LOGIN),
+                email: row.get(COL_INDEX_ACCOUNT_EMAIL),
                 id: UserID(user_id),
                 role: user_role,
-                password: row.get("password"),
+                password: row.get(COL_INDEX_USER_PASSWORD), // todo: hide this later
                 registered: registered.to_string(),
             }
         })
