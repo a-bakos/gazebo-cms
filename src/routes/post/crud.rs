32fb221b5ae77af2090ee1df7a786e2a6e35ad92
a-bakos <bakosattila@fastem.com>
2023-06-01 20:28:44 +0100

    Add placeholder post crud functions

--- src/routes/post/crud.rs
+++ src/routes/post/crud.rs
@@ -1,7 +1,22 @@
 use sqlx::PgPool;
 use std::collections::HashMap;
 
+pub fn get_post(pool: PgPool, params: HashMap<String, String>) {
+    // -> Result<impl warp::Reply, warp::Rejection> {
+    todo!();
+}
+
 pub fn insert_post(pool: PgPool, params: HashMap<String, String>) {
+    // -> Result<impl warp::Reply, warp::Rejection> {
+    todo!();
+}
+
+pub fn update_post(pool: PgPool, params: HashMap<String, String>) {
+    // -> Result<impl warp::Reply, warp::Rejection> {
+    todo!();
+}
+
+pub fn delete_post(pool: PgPool, params: HashMap<String, String>) {
     // -> Result<impl warp::Reply, warp::Rejection> {
     todo!();
 }
