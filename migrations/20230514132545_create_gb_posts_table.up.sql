5378f409a9fa899eb0a5b5d5fe5c642a4bf3d421
a-bakos <bakosattila@fastem.com>
2023-06-08 20:39:25 +0100

    Update in posts table migrations file

--- migrations/20230514132545_create_gb_posts_table.up.sql
+++ migrations/20230514132545_create_gb_posts_table.up.sql
@@ -5,10 +5,11 @@ CREATE TABLE IF NOT EXISTS gb_posts (
     date_publish TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     date_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     slug VARCHAR(255) NOT NULL,
-    post_type VARCHAR(20) DEFAULT 'post',
+    --post_type VARCHAR(20) DEFAULT 'post', -- type is not needed if table is called "posts"
     title VARCHAR(255) NOT NULL,
     excerpt TEXT DEFAULT NULL,
     content TEXT NOT NULL,
     password VARCHAR(255) DEFAULT NULL,
     status VARCHAR(20) DEFAULT 'draft'
+    --tags INTEGER[] DEFAULT NULL -- todo
 );
