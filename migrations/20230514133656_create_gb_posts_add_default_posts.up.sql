51e20e17e7671e166bbd788c58de5483d8e7c051
a-bakos <bakosattila@fastem.com>
2023-06-08 20:40:05 +0100

    Update in post defaults table migrations file

--- migrations/20230514133656_create_gb_posts_add_default_posts.up.sql
+++ migrations/20230514133656_create_gb_posts_add_default_posts.up.sql
@@ -1,8 +1,8 @@
-INSERT INTO gb_posts (author_id, title, content, post_type, slug, status)
-SELECT 1, 'Hello, Gazebo!', 'First demo post for your Gazebo CMS.', 'post', 'hello-gazebo', 'publish'
+INSERT INTO gb_posts (author_id, title, content, slug, status)
+SELECT 1, 'Hello, Gazebo!', 'First demo post for your Gazebo CMS.', 'hello-gazebo', 'publish'
 UNION ALL
-SELECT 1, 'A CMS experiment project', 'Second demo post for your Gazebo CMS.', 'post', 'cms-experiment', 'draft'
+SELECT 1, 'A CMS experiment project', 'Second demo post for your Gazebo CMS.', 'cms-experiment', 'draft'
 UNION ALL
-SELECT 1, 'Inspired by WordPress', 'Third demo post for your Gazebo CMS.', 'post', 'inspired-by-wp', 'private'
+SELECT 1, 'Inspired by WordPress', 'Third demo post for your Gazebo CMS.', 'inspired-by-wp', 'private'
 WHERE NOT EXISTS (SELECT id FROM gb_posts WHERE title IN ('Hello, Gazebo!', 'A CMS experiment project', 'Inspired by WordPress'))
 RETURNING id;
