0448e79a4afa791de210110589e2c55b43d0815a
a-bakos <bakosattila@fastem.com>
2023-06-09 22:05:28 +0100

    Modelling GB_Media

--- src/entry/media.rs
+++ src/entry/media.rs
@@ -1,1 +1,14 @@
-struct GB_Media {}
+use crate::entry::post::EntryID;
+use crate::users::user::UserID;
+
+struct GB_Media {
+    id: EntryID,
+    uploader: UserID,
+    attached_to: Vec<EntryID>,
+    date_publish: String,
+    date_modified: String,
+    slug: String,
+    title: String,
+    description: Option<String>,
+    alt_text: Option<String>,
+}
--- src/entry/mod.rs
+++ src/entry/mod.rs
@@ -1,6 +1,7 @@
 pub mod consts;
 pub mod entry_type;
 pub mod functions;
+pub mod media;
 pub mod post;
 pub mod query;
 pub mod status;
