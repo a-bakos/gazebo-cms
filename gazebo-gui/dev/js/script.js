bc8d210e1cd22f8ee0a2994e470566b1c9f3cf9f
a-bakos <bakosattila@fastem.com>
2023-06-20 20:31:39 +0100

    Working on basic HTML elements (front end experimentation)

--- gazebo-gui/dev/js/script.js
+++
@@ -1,1 +0,0 @@
-console.log("Hello Gazebo script!")
--- gazebo-gui/src/app.rs
+++ gazebo-gui/src/app.rs
@@ -1,12 +1,37 @@
 use yew::prelude::*;
 
+#[function_component(Header)]
+pub fn header() -> Html {
+    html! {
+        <>
+        <h1>{ "Hello, Gazebo CMS!" }</h1>
+        <MainNavigation />
+        </>
+    }
+}
+
+#[function_component(MainNavigation)]
+pub fn main_navigation() -> Html {
+    html! {
+        <nav>
+            <ul>
+                <li>{ "Menu 1" }</li>
+                <li>{ "Menu 2" }</li>
+                <li>{ "Menu 3" }</li>
+            </ul>
+        </nav>
+    }
+}
+
 #[function_component(App)]
 pub fn app() -> Html {
     html! {
+        <>
+        <Header />
         <main>
-            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
+            <img class="logo" src="assets/gazebo-logo.jpg" alt="Gazebo logo" />
             <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
         </main>
+        </>
     }
 }
