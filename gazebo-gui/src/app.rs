640ac43c803d8228e0324589705ff50eb4f2cac2
a-bakos <bakosattila@fastem.com>
2023-06-26 18:49:44 +0100

    Start building a Yew Router

--- gazebo-gui/src/app.rs
+++ gazebo-gui/src/app.rs
@@ -1,10 +1,28 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
+
+use crate::pages::admin::Admin;
+use crate::pages::home::Home;
+use crate::pages::login::Login;
 
 use crate::components::admin_bar::AdminBar;
 use crate::components::footer::Footer;
 use crate::components::header::Header;
 use crate::components::login_form::LoginForm;
 use crate::components::post_rollup::PostRollup;
+
+#[derive(Clone, PartialEq, Routable)]
+pub enum Route {
+    #[at("/")]
+    Home,
+    #[at("/login")]
+    Login,
+    #[at("/admin")]
+    Admin,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
 
 #[function_component(App)]
 pub fn app() -> Html {
