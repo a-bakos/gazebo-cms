a83931e654e4bc120c422988f30f5adeb09da56a
a-bakos <bakosattila@fastem.com>
2023-06-26 18:44:12 +0100

    Add wasm console logging to front end

--- gazebo-gui/src/main.rs
+++ gazebo-gui/src/main.rs
@@ -5,5 +5,7 @@ mod components;
 use app::App;
 
 fn main() {
+    // To create console logs, you can now use the log::info!(“”); macro
+    wasm_logger::init(wasm_logger::Config::default());
     yew::Renderer::<App>::new().render();
 }
