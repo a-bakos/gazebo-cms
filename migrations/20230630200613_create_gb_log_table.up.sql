d47bc966e14bee97582546d6066bdf18eb04fbc1
a-bakos <bakosattila@fastem.com>
2023-06-30 21:19:39 +0100

    Add Log table migrations file

---
+++ migrations/20230630200613_create_gb_log_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS gb_log (
+    id serial PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    user_email VARCHAR(100) NOT NULL,
+    event_code INTEGER NOT NULL,
+    event_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    title VARCHAR(255) NOT NULL,
+    description TEXT DEFAULT NULL,
+);
