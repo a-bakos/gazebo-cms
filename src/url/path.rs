744138af947d50b352b7567a5fcce76e53628add
a-bakos <bakosattila@fastem.com>
2023-06-30 22:20:25 +0100

    Review endpoint paths

--- src/url/path.rs
+++ src/url/path.rs
@@ -1,8 +1,9 @@
+// Top level paths
+pub const PATH_POST: &str = "post";
 pub const PATH_USER: &str = "user";
-pub const PATH_USER_REGISTRATION: &str = "registration";
 pub const PATH_USER_LOGIN: &str = "login";
 
-pub const PATH_POST: &str = "post";
-
+// Second level paths
+pub const PATH_USER_REGISTRATION: &str = "registration"; // todo use the add below instead of reg
 #[allow(dead_code)]
-pub const PATH_POST_ADD: &str = "add_post";
+pub const PATH_ADD: &str = "add"; // eg post/add or user/add
