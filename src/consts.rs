8e9525aea5521fbe968a9214be3b1d73dff5524e
a-bakos <bakosattila@fastem.com>
2023-07-21 18:02:37 +0100

    Add comments to annotate Entry Types

--- src/consts.rs
+++ src/consts.rs
@@ -38,11 +38,17 @@ pub const USER_ROLE_EDITOR: &str = "editor";
 pub const USER_ROLE_CONTRIBUTOR: &str = "contributor";
 pub const USER_ROLE_NOT_FOUND: &str = "not_found";
 
+/// Entry type for blog-like posts
 pub const ENTRY_TYPE_POST: &str = "post";
+/// Entry type for top-level pages
 pub const ENTRY_TYPE_PAGE: &str = "page";
+/// Entry type for media items
 pub const ENTRY_TYPE_MEDIA: &str = "media";
+/// Entry type for logged events
 pub const ENTRY_TYPE_LOG: &str = "log";
+/// Entry type for search records
 pub const ENTRY_TYPE_SEARCH: &str = "search";
+/// Unknown entry type group
 pub const ENTRY_TYPE_UNKNOWN: &str = "unknown";
 
 pub const ENTRY_ID_NO_PARENT: i32 = 0;
