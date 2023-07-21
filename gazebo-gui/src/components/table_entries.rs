a25236593a6ad452f1af405d564e91f12f54bd70
a-bakos <bakosattila@fastem.com>
2023-07-21 20:58:42 +0100

    Optimize post title fetching and rendering in entry table

--- gazebo-gui/src/components/table_entries.rs
+++ gazebo-gui/src/components/table_entries.rs
@@ -3,81 +3,60 @@ use yew::prelude::*;
 use yew::{platform::spawn_local, prelude::*};
 
 #[derive(Clone, PartialEq, Deserialize)]
-struct PostTitles {
-    titles: Vec<String>,
-}
-
-#[derive(Clone, PartialEq, Deserialize)]
-struct TableRows {
-    rows: Vec<EntryTableRow>,
-}
-
-#[derive(Clone, PartialEq, Deserialize)]
 struct EntryTableRow {
     title: String,
-    status: String,
-    author: String,
-    category: Option<Vec<String>>,
-    date: String,
-    id: u32,
+    // status: String,
+    // author: String,
+    // category: Option<Vec<String>>,
+    // date: String,
+    // id: u32,
 }
 
 fn table_entry_row(row_data: EntryTableRow) -> Html {
     html! {
         <tr>
-            <td>{row_data.title.clone()}</td>
-            <td>{row_data.status.clone()}</td>
-            <td>{row_data.author.clone()}</td>
-            <td>{"None"}</td> // category
-            <td>{row_data.date.clone()}</td>
+            <td>
+                <a
+                    title={ row_data.title.clone() }
+                    href="#">
+                    { row_data.title.clone() }
+                </a>
+            </td>
+            // <td>{row_data.status.clone()}</td>
+            // <td>{row_data.author.clone()}</td>
+            // <td>{"None"}</td> // category
+            // <td>{row_data.date.clone()}</td>
         </tr>
     }
 }
 
 #[function_component(EntriesTable)]
 pub fn table_entries() -> Html {
-    let mut table_rows = TableRows {
-        rows: vec![EntryTableRow {
-            title: "Post title 1".to_string(),
-            status: "publish".to_string(),
-            author: "author1".to_string(),
-            category: Some(vec![
-                "category1".to_string(),
-                "category2".to_string(),
-                "category3".to_string(),
-            ]),
-            date: "2023-06-29 21:27".to_string(),
-            id: 1,
-        }],
-    };
+    let row_titles_handle = use_state(|| Vec::<EntryTableRow>::new());
+    let row_titles = row_titles_handle.clone();
+    use_effect_with_deps(
+        move |_| {
+            spawn_local(async move {
+                let mut entry_rows: Vec<EntryTableRow> = vec![];
+                let response = crate::api::post::api_get_all_posts().await.unwrap();
+                // todo - now make this work with the full post structure
+                for title in response.iter() {
+                    gloo_console::log!("response: ", title);
 
-    //let row_titles: Vec<PostTitles> = vec![];
-
-    let row_titles_handle: UseStateHandle<Vec<String>> = use_state(|| vec![]);
-    {
-        let row_titles_handle = row_titles_handle.clone();
-        use_effect_with_deps(
-            move |_| {
-                spawn_local(async move {
-                    let response = crate::api::post::api_get_all_posts().await.unwrap();
-                    for title in response.iter() {
-                        gloo_console::log!("response: ", title);
-                    }
-                    row_titles_handle.set(response);
-                });
-                || ()
-            },
-            (),
-        );
-    }
+                    let entry_row = EntryTableRow {
+                        title: title.to_string(),
+                    };
+                    entry_rows.push(entry_row);
+                }
+                row_titles_handle.set(entry_rows);
+            });
+            || ()
+        },
+        (),
+    );
 
     html! {
         <>
-          // {
-          //   for response.iter().map(|title| html! {
-          //     <p>{title.clone()}</p>
-          //   } )
-          // }
             <table class={"gb-admin-table"}>
                 <thead>
                     <tr>
@@ -90,37 +69,8 @@ pub fn table_entries() -> Html {
                 </thead>
                 <tbody>
                     {
-                        for row_titles_handle.iter().map(|row| html! {
-                            <tr>
-                                <td>
-                                    <a
-                                        title={ row.clone() }
-                                        href="">{ row.clone() }</a>
-                                </td>
-                                <td></td>//{ &row.status }</td>
-                                <td></td>//{ &row.author }</td>
-                                <td>
-                                    // {
-                                    //     if let Some(categories) = &row.category {
-                                    //         html! {
-                                    //             <>
-                                    //                 {
-                                    //                     for categories.iter().map(|tag| html! {
-                                    //                         <>
-                                    //                             <span>{ tag }</span>
-                                    //                             <br />
-                                    //                         </>
-                                    //                     } )
-                                    //                 }
-                                    //             </>
-                                    //         }
-                                    //     } else {
-                                    //         html! { <></> }
-                                    //     }
-                                    // }
-                                </td>
-                                <td></td>//{ &row.date }</td>
-                            </tr>
+                        for row_titles.iter().map(|entry_row| html! {
+                            table_entry_row(entry_row.clone())
                         } )
                     }
                 </tbody>
