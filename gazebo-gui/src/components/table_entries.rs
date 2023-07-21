8611bb5d2c6b8ea7401cce7c2bca52ba851836ff
a-bakos <bakosattila@fastem.com>
2023-07-21 20:08:52 +0100

    Yew: Fetching post titles from db into admin entry column

--- gazebo-gui/src/components/table_entries.rs
+++ gazebo-gui/src/components/table_entries.rs
@@ -1,10 +1,18 @@
+use serde::Deserialize;
 use yew::prelude::*;
 use yew::{platform::spawn_local, prelude::*};
 
+#[derive(Clone, PartialEq, Deserialize)]
+struct PostTitles {
+    titles: Vec<String>,
+}
+
+#[derive(Clone, PartialEq, Deserialize)]
 struct TableRows {
     rows: Vec<EntryTableRow>,
 }
 
+#[derive(Clone, PartialEq, Deserialize)]
 struct EntryTableRow {
     title: String,
     status: String,
@@ -29,46 +37,39 @@ fn table_entry_row(row_data: EntryTableRow) -> Html {
 #[function_component(EntriesTable)]
 pub fn table_entries() -> Html {
     let mut table_rows = TableRows {
-        rows: vec![
-            EntryTableRow {
-                title: "Post title 1".to_string(),
-                status: "publish".to_string(),
-                author: "author1".to_string(),
-                category: Some(vec![
-                    "category1".to_string(),
-                    "category2".to_string(),
-                    "category3".to_string(),
-                ]),
-                date: "2023-06-29 21:27".to_string(),
-                id: 1,
-            },
-            EntryTableRow {
-                title: "Post title 2".to_string(),
-                status: "publish".to_string(),
-                author: "author2".to_string(),
-                category: None,
-                date: "2023-06-28 21:27".to_string(),
-                id: 2,
-            },
-            EntryTableRow {
-                title: "Post title 3".to_string(),
-                status: "publish".to_string(),
-                author: "author3".to_string(),
-                category: Some(vec!["category1".to_string()]),
-                date: "2023-06-27 21:27".to_string(),
-                id: 3,
-            },
-        ],
+        rows: vec![EntryTableRow {
+            title: "Post title 1".to_string(),
+            status: "publish".to_string(),
+            author: "author1".to_string(),
+            category: Some(vec![
+                "category1".to_string(),
+                "category2".to_string(),
+                "category3".to_string(),
+            ]),
+            date: "2023-06-29 21:27".to_string(),
+            id: 1,
+        }],
     };
 
-    // TODO
-    spawn_local(async move {
-        let response = crate::api::post::api_get_all_posts().await.unwrap();
+    //let row_titles: Vec<PostTitles> = vec![];
 
-        for title in response.iter() {
-            gloo_console::log!("response: ", title);
-        }
-    });
+    let row_titles_handle: UseStateHandle<Vec<String>> = use_state(|| vec![]);
+    {
+        let row_titles_handle = row_titles_handle.clone();
+        use_effect_with_deps(
+            move |_| {
+                spawn_local(async move {
+                    let response = crate::api::post::api_get_all_posts().await.unwrap();
+                    for title in response.iter() {
+                        gloo_console::log!("response: ", title);
+                    }
+                    row_titles_handle.set(response);
+                });
+                || ()
+            },
+            (),
+        );
+    }
 
     html! {
         <>
@@ -89,36 +90,36 @@ pub fn table_entries() -> Html {
                 </thead>
                 <tbody>
                     {
-                        for table_rows.rows.iter().map(|row| html! {
+                        for row_titles_handle.iter().map(|row| html! {
                             <tr>
                                 <td>
                                     <a
-                                        title={ row.title.clone() }
-                                        href="">{ row.title.clone() }</a>
+                                        title={ row.clone() }
+                                        href="">{ row.clone() }</a>
                                 </td>
-                                <td>{ &row.status }</td>
-                                <td>{ &row.author }</td>
+                                <td></td>//{ &row.status }</td>
+                                <td></td>//{ &row.author }</td>
                                 <td>
-                                    {
-                                        if let Some(categories) = &row.category {
-                                            html! {
-                                                <>
-                                                    {
-                                                        for categories.iter().map(|tag| html! {
-                                                            <>
-                                                                <span>{ tag }</span>
-                                                                <br />
-                                                            </>
-                                                        } )
-                                                    }
-                                                </>
-                                            }
-                                        } else {
-                                            html! { <></> }
-                                        }
-                                    }
+                                    // {
+                                    //     if let Some(categories) = &row.category {
+                                    //         html! {
+                                    //             <>
+                                    //                 {
+                                    //                     for categories.iter().map(|tag| html! {
+                                    //                         <>
+                                    //                             <span>{ tag }</span>
+                                    //                             <br />
+                                    //                         </>
+                                    //                     } )
+                                    //                 }
+                                    //             </>
+                                    //         }
+                                    //     } else {
+                                    //         html! { <></> }
+                                    //     }
+                                    // }
                                 </td>
-                                <td>{ &row.date }</td>
+                                <td></td>//{ &row.date }</td>
                             </tr>
                         } )
                     }
