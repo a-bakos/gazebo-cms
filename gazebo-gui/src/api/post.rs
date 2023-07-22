436cd3fc5e5fa3bfdd619cc37a3b46c1015b8680
a-bakos <bakosattila@fastem.com>
2023-07-22 20:50:56 +0100

    WIP Map API response to GB_Post on the front end

--- gazebo-gui/src/api/post.rs
+++ gazebo-gui/src/api/post.rs
@@ -3,6 +3,7 @@ use gloo_net::http::Request;
 use serde::Deserialize;
 use serde_json::json;
 
+#[derive(Deserialize, Debug, PartialEq)]
 pub struct GB_Post {
     pub id: u32,
     pub id_author: u32,
@@ -10,7 +11,7 @@ pub struct GB_Post {
     pub date_publish: String,
     pub date_modified: String,
     pub slug: Option<String>,
-    pub status: String,
+    //pub status: String, // todo
     pub title: Option<String>,
     pub excerpt: Option<String>,
     pub content: Option<String>,
@@ -19,9 +20,10 @@ pub struct GB_Post {
 
 pub async fn api_get_all_posts() -> Result<Vec<GB_Post>, gloo_net::Error> {
     // gloo-net
+
+    // todo : turn response into Vec<GB_Post>
     let response = Request::get(&format!("{}/posts", BACKEND_URL_BASE))
         .send()
         .await?;
-
     response.json::<Vec<GB_Post>>().await
 }
--- gazebo-gui/src/components/table_entries.rs
+++ gazebo-gui/src/components/table_entries.rs
@@ -5,13 +5,19 @@ use yew::{platform::spawn_local, prelude::*};
 // TODO - rename this to Post and create a separate file for the Page type
 
 #[derive(Clone, PartialEq, Deserialize)]
-struct EntryTableRow {
-    title: String,
-    status: String,
-    author: String,
-    category: Option<Vec<String>>,
-    date: String,
-    id: u32,
+pub struct EntryTableRow {
+    //category: Option<Vec<String>>,
+    pub id: u32,
+    pub id_author: u32,
+    pub id_parent: Option<u32>,
+    pub date_publish: String,
+    pub date_modified: String,
+    pub slug: Option<String>,
+    //pub status: String, // todo
+    pub title: Option<String>,
+    pub excerpt: Option<String>,
+    pub content: Option<String>,
+    pub password: Option<String>,
 }
 
 fn table_entry_row(row_data: &EntryTableRow) -> Html {
@@ -24,10 +30,10 @@ fn table_entry_row(row_data: &EntryTableRow) -> Html {
                     { row_data.title.clone() } { row_data.id.clone() }
                 </a>
             </td>
-            <td>{row_data.status.clone()}</td>
-            <td>{row_data.author.clone()}</td>
+            <td></td>//{row_data.status.clone()}</td>
+            <td>{row_data.id_author.clone()}</td>
             <td>{"Category TBC"}</td>
-            <td>{row_data.date.clone()}</td>
+            <td>{row_data.date_publish.clone()}</td>
         </tr>
     }
 }
@@ -42,16 +48,21 @@ pub fn table_entries() -> Html {
                 let mut entry_rows: Vec<EntryTableRow> = vec![];
                 let response = crate::api::post::api_get_all_posts().await.unwrap();
                 // todo - now make this work with the full post structure
-                for tuple in response.iter() {
-                    gloo_console::log!("response: ", tuple.0, tuple.1.clone());
+                for response_gb_post in response.iter() {
+                    gloo_console::log!("response: ", response_gb_post.title.clone());
 
                     let entry_row = EntryTableRow {
-                        title: tuple.1.clone(),
-                        status: "status".to_string(),
-                        author: "author".to_string(),
-                        category: Some(vec!["cat".to_string()]),
-                        date: "date".to_string(),
-                        id: tuple.0,
+                        id: response_gb_post.id,
+                        id_author: response_gb_post.id_author,
+                        id_parent: response_gb_post.id_parent,
+                        date_publish: response_gb_post.date_publish.clone(),
+                        date_modified: response_gb_post.date_modified.clone(),
+                        slug: response_gb_post.slug.clone(),
+                        //status: response_gb_post.status.clone(),
+                        title: response_gb_post.title.clone(),
+                        excerpt: response_gb_post.excerpt.clone(),
+                        content: response_gb_post.content.clone(),
+                        password: response_gb_post.password.clone(),
                     };
                     entry_rows.push(entry_row);
                 }
