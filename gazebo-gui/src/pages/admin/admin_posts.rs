5d22967661b918c80f8922291cd71488704e6a70
a-bakos <bakosattila@fastem.com>
2023-08-08 20:57:05 +0100

    Working on Entries Table

--- gazebo-gui/src/pages/admin/admin_posts.rs
+++ gazebo-gui/src/pages/admin/admin_posts.rs
@@ -13,12 +13,29 @@ pub fn admin_posts() -> Html {
         <main id={crate::consts::CSS_ID_ADMIN_AREA}>
             <AdminBar />
 
-            <div class={"flex bg-red-100"}>
-                <div class={"w-full"}>
-                    <h1>{"Posts"}</h1>
-                    <ButtonAddNewEntry />
+            <div class={"flex"}>
+                <nav class="bg-gray-300">
+                    <ul class="w-max">
+                        <li><button class="w-full bg-blue-400 px-4 block" href="">{"?Category"}</button></li>
+                        <li><button class="w-full bg-blue-400 px-4 block" href="">{"?Settings"}</button></li>
+                        <li><button class="w-full bg-blue-400 px-4 block" href="">{ "3" }</button></li>
+                        <li><button class="w-full bg-blue-400 px-4 block" href="">{ "4" }</button></li>
+                        <li><button class="w-full bg-blue-400 px-4 block" href="">{ "5" }</button></li>
+                    </ul>
+                </nav>
+                <section class="bg-white w-full p-4">
+                    <h2 class="font-bold ">{"POST ENTRIES"}</h2>
+                    <div class="flex w-full bg-gray-100 border">
+                        <ButtonAddNewEntry />
+                        <button class="bg-blue-200 px-4 py-2 mr-px hover:bg-blue-500">{"Publish"}</button>
+                        <button class="bg-blue-200 px-4 py-2 mr-px hover:bg-blue-500">{"Draft"}</button>
+                        <button class="bg-blue-200 px-4 py-2 mr-px hover:bg-blue-500">{"Bin"}</button>
+                        <input type="text" class="px-4 py-2 mr-px" />
+                        <button class="bg-blue-200 px-4 py-2 mr-px hover:bg-blue-500">{"Search"}</button>
+                        <button class="bg-blue-200 px-4 py-2 mr-px hover:bg-blue-500">{"Export"}</button>
+                    </div>
                     <EntriesTable />
-                </div>
+                </section>
             </div>
 
         </main>
