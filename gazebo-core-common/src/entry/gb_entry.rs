e2d56ab8dc50a077c9cb8eda73916458fee35493
a-bakos <bakosattila@fastem.com>
2023-08-29 18:52:30 +0100

    Move impl blocks

--- gazebo-core-common/src/entry/gb_entry.rs
+++ gazebo-core-common/src/entry/gb_entry.rs
@@ -4,12 +4,12 @@ use crate::entry::gb_media::GB_Media;
 use crate::entry::gb_post::GB_Post;
 use crate::entry::status::{EntryStatus, MediaStatus};
 
-enum GB_EntryDateVariant {
+pub enum GB_EntryDateVariant {
     Publish,
     Modified,
 }
 
-trait GB_Entry {
+pub trait GB_EntryCommon {
     fn get_id(&self) -> EntryID;
 
     fn get_author_id(&self) -> AccountID;
@@ -23,80 +23,13 @@ trait GB_Entry {
     fn get_title(&self) -> String;
 }
 
-impl GB_Entry for GB_Post {
-    fn get_id(&self) -> EntryID {
-        self.id
-    }
-
-    fn get_author_id(&self) -> AccountID {
-        self.id_author.clone()
-    }
-
-    fn get_date(&self, date_variant: GB_EntryDateVariant) -> String {
-        match date_variant {
-            GB_EntryDateVariant::Publish => self.date_publish.clone(),
-            GB_EntryDateVariant::Modified => self.date_modified.clone(),
-        }
-    }
-
-    fn get_slug(&self) -> String {
-        if self.slug.is_some() {
-            return self.slug.clone().unwrap();
-        }
-        "".to_string()
-    }
-
-    fn get_status(&self) -> EntryStatus {
-        self.status.clone()
-    }
-
-    fn get_title(&self) -> String {
-        if self.title.is_some() {
-            return self.title.clone().unwrap();
-        }
-        "".to_string()
-    }
-}
-
-impl GB_Entry for GB_Media {
-    fn get_id(&self) -> EntryID {
-        self.id
-    }
-
-    fn get_author_id(&self) -> AccountID {
-        self.uploader.clone()
-    }
-
-    fn get_date(&self, date_variant: GB_EntryDateVariant) -> String {
-        match date_variant {
-            GB_EntryDateVariant::Publish => self.date_publish.clone(),
-            GB_EntryDateVariant::Modified => self.date_modified.clone(),
-        }
-    }
-
-    fn get_slug(&self) -> String {
-        self.slug.clone()
-    }
-
-    fn get_status(&self) -> EntryStatus {
-        if self.attached_to.is_empty() {
-            return EntryStatus::Media(MediaStatus::Unattached);
-        }
-        EntryStatus::Media(MediaStatus::Attached)
-    }
-
-    fn get_title(&self) -> String {
-        self.title.clone()
-    }
-}
-
 // A generic example
-fn get_id<T: GB_Entry>(entry: &T) -> EntryID {
+fn get_id<T: GB_EntryCommon>(entry: &T) -> EntryID {
     entry.get_id()
 }
 
 fn temp() {
-    let entries: Vec<Box<dyn GB_Entry>> = vec![
+    let entries: Vec<Box<dyn GB_EntryCommon>> = vec![
         Box::new(GB_Post {
             id: Default::default(),
             id_author: Default::default(),
--- gazebo-core-common/src/entry/gb_post.rs
+++ gazebo-core-common/src/entry/gb_post.rs
@@ -3,6 +3,7 @@ use crate::{
     datetime::get_current_date,
     entry::{
         entry_id::{get_entry_parent_id, EntryID},
+        gb_entry::{GB_EntryCommon, GB_EntryDateVariant},
         status::{ContentStatus, EntryStatus},
     },
 };
@@ -25,3 +26,38 @@ pub struct GB_Post {
 }
 
 impl GB_Post {}
+
+impl GB_EntryCommon for GB_Post {
+    fn get_id(&self) -> EntryID {
+        self.id
+    }
+
+    fn get_author_id(&self) -> AccountID {
+        self.id_author.clone()
+    }
+
+    fn get_date(&self, date_variant: GB_EntryDateVariant) -> String {
+        match date_variant {
+            GB_EntryDateVariant::Publish => self.date_publish.clone(),
+            GB_EntryDateVariant::Modified => self.date_modified.clone(),
+        }
+    }
+
+    fn get_slug(&self) -> String {
+        if self.slug.is_some() {
+            return self.slug.clone().unwrap();
+        }
+        "".to_string()
+    }
+
+    fn get_status(&self) -> EntryStatus {
+        self.status.clone()
+    }
+
+    fn get_title(&self) -> String {
+        if self.title.is_some() {
+            return self.title.clone().unwrap();
+        }
+        "".to_string()
+    }
+}
