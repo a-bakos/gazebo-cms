19e3fb03080d1ee831b601a361c9d1d18b09c853
a-bakos <bakosattila@fastem.com>
2023-08-30 22:25:38 +0100

    Redirect to post editor after successful insertion

--- gazebo-gui/src/pages/edit_entry_new.rs
+++ gazebo-gui/src/pages/edit_entry_new.rs
@@ -7,20 +7,25 @@ use crate::components::{admin_bar::AdminBar, entry_editor::EntryEditor};
 use gazebo_core_common::entry::gb_post::GB_Post;
 
 use crate::api::post::api_new_entry_insert_request;
+use crate::app::MainNavigationRoute;
 use gazebo_core_common::account::gb_account::AccountID;
 use gazebo_core_common::entry::entry_id::EntryID;
+use gazebo_core_common::entry::entry_type::EntryType;
 use gazebo_core_common::entry::gb_entry::{GB_EntryInsertRequest, GB_EntryInsertResponse};
 use gazebo_core_common::entry::status::ContentStatus::Draft;
 use gazebo_core_common::entry::status::EntryStatus;
 use yew::html::IntoPropValue;
 use yew::platform::spawn_local;
 use yew::prelude::*;
+use yew_router::prelude::use_navigator;
 
 #[function_component(EntryEdit)]
 pub fn entry_edit() -> Html {
-    //let navigator = use_navigator();
+    let navigator = use_navigator();
     let on_form_submit = Callback::from(move |event: SubmitEvent| {
         event.prevent_default();
+
+        let clone_navigator = navigator.clone();
 
         // Console logging for now
         gloo_console::log!("Saving new post: ");
@@ -44,14 +49,16 @@ pub fn entry_edit() -> Html {
                     gloo_console::log!(
                         "Entry successfully added: ",
                         response.http_status_code,
-                        response.entry_id.0
+                        response.entry_id.0 // TODO this value is sticky, fix it
                     );
 
-                    // Redirect to Home
-                    //if let Some(nav) = clone_navigator {
-                    // Redirect to post edit page using the returned ID
-                    //nav.push(&MainNavigationRoute::Home)
-                    //}
+                    if let Some(nav) = clone_navigator {
+                        // Redirect to post edit page using the returned ID
+                        nav.push(&MainNavigationRoute::EntryEditExisting {
+                            entry_type: EntryType::Post.to_string(),
+                            id: response.entry_id.0.to_string(),
+                        })
+                    }
                 }
                 _ => {
                     gloo_console::log!("Error during saving entry!");
