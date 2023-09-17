8bf7307fe777f8ee81dd6cbbffd2d75eacd6e2cb
a-bakos <bakosattila@fastem.com>
2023-09-17 17:55:34 +0100

    Prototyping GB_Log types and methods

--- gazebo-core-common/src/entry/gb_log.rs
+++ gazebo-core-common/src/entry/gb_log.rs
@@ -1,6 +1,31 @@
 use crate::account::gb_account::AccountID;
-use crate::entry::entry_id::EntryID;
-use serde::{Deserialize, Serialize};
+
+pub enum GB_EventCode {
+    // Account events
+    AccountRegistered = 100,
+    AccountLogin = 101,
+    AccountLogout = 102,
+    AccountKicked = 103,
+    AccountRemoved = 104,
+    AccountRoleChanged = 105,
+    AccountEmailChanged = 106,
+    AccountPasswordChanged = 107,
+    // Entry events
+    EntryAdded = 200,
+    EntryStatusChanged = 201,
+    EntryTrashed = 202,
+    EntryUntrashed = 203,
+    EntryDeleted = 204,
+    // Category events
+    CategoryAdded = 300,
+    CategoryChanged = 301,
+    CategoryRemoved = 302,
+    // File
+    FileUploaded = 400,
+    FileDeleted = 401,
+    // Other
+    Unknown = 999,
+}
 
 //#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 #[allow(non_camel_case_types)]
@@ -21,23 +46,52 @@ pub struct GB_Log {
     pub subject_to: Option<String>,
 }
 
-pub enum GB_EventCode {
-    // Account events
-    AccountRegistered = 100,
-    AccountLogin = 101,
-    AccountLogout = 102,
-    AccountKicked = 103,
-    AccountRemoved = 104,
-    AccountRoleChanged = 105,
-    AccountEmailChanged = 106,
-    AccountPasswordChanged = 107,
-    // Entry events
-    EntryAdded = 200,
-    EntryStatusChanged = 201,
-    EntryTrashed = 202,
-    EntryDeleted = 203,
-    // Category events
-    CategoryAdded = 300,
-    CategoryChanged = 301,
-    CategoryRemoved = 302,
+impl GB_Log {
+    pub fn get_event_description(&self) -> String {
+        match self.event_code {
+            GB_EventCode::AccountRegistered => "Registered an account".to_string(),
+            GB_EventCode::AccountLogin => "Logged in".to_string(),
+            GB_EventCode::AccountLogout => "Logged out".to_string(),
+            GB_EventCode::AccountKicked => "Kicked a user out".to_string(),
+            GB_EventCode::AccountRemoved => "Deleted an account".to_string(),
+            GB_EventCode::AccountRoleChanged => "Changed account role".to_string(),
+            GB_EventCode::AccountEmailChanged => "Changed account email".to_string(),
+            GB_EventCode::AccountPasswordChanged => "Changed password".to_string(),
+            GB_EventCode::EntryAdded => "Deleted an entry".to_string(),
+            GB_EventCode::EntryStatusChanged => "Changed entry status".to_string(),
+            GB_EventCode::EntryTrashed => "Moved an entry to the trash".to_string(),
+            GB_EventCode::EntryUntrashed => "Restored an entry from the trash".to_string(),
+            GB_EventCode::EntryDeleted => "Permanently delete an entry".to_string(),
+            GB_EventCode::CategoryAdded => "Created a category".to_string(),
+            GB_EventCode::CategoryChanged => "Edited a category".to_string(),
+            GB_EventCode::CategoryRemoved => "Deleted a category".to_string(),
+            GB_EventCode::FileUploaded => "Uploaded a file".to_string(),
+            GB_EventCode::FileDeleted => "Deleted a file".to_string(),
+            GB_EventCode::Unknown => "Unknown event".to_string(),
+        }
+    }
+}
+
+fn is_admin_only_event(event_code: GB_EventCode) -> bool {
+    match event_code {
+        GB_EventCode::AccountRegistered => true,
+        GB_EventCode::AccountLogin => false,
+        GB_EventCode::AccountLogout => false,
+        GB_EventCode::AccountKicked => true,
+        GB_EventCode::AccountRemoved => true,
+        GB_EventCode::AccountRoleChanged => true,
+        GB_EventCode::AccountEmailChanged => true,
+        GB_EventCode::AccountPasswordChanged => true,
+        GB_EventCode::EntryAdded => false,
+        GB_EventCode::EntryStatusChanged => false,
+        GB_EventCode::EntryTrashed => false,
+        GB_EventCode::EntryUntrashed => false,
+        GB_EventCode::EntryDeleted => false,
+        GB_EventCode::CategoryAdded => false,
+        GB_EventCode::CategoryChanged => false,
+        GB_EventCode::CategoryRemoved => false,
+        GB_EventCode::FileUploaded => false,
+        GB_EventCode::FileDeleted => false,
+        GB_EventCode::Unknown => false,
+    }
 }
